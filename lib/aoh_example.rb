ba38a28384affe29c0102c983df54fe30d59917d
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-26 01:59:48 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,5 +31,8 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  literal_aoh[1][:name] = "Joelle van Dyne (PGOAT)"
+  aoh_update = [
+    literal_aoh[1][:name] = "Joelle van Dyne (PGOAT)"]
+  ]
+return aoh_update
 end
