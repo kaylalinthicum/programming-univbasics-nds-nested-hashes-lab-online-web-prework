996693b76e2dfd7859e134fceba3856f220b30be
Kayla Linthicum <kaylalinthicum@gmail.com>
2019-11-26 02:13:05 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -31,5 +31,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-    aoh_update = [aoh][row][key] = "new_value"
+    aoh_update =[
+    [aoh][row][key] = "new_value"
+    ]
 end
